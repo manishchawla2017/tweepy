dd35ffb61ed0297ac8d631ca642bdd13e5f3c098
manishchawla2017 <er.mchawla@gmail.com>
2017-10-10 00:07:01 +0530

    minor changes

--- search.py
+++ search.py
@@ -2,7 +2,9 @@ import login
 import functs
 import tweepy
 import csv
-for status in tweepy.Cursor(login.api.search,q="isro").items(10):
+itemCount=20
+query=['isro']
+for status in tweepy.Cursor(login.api.search,q=query).items(itemCount):
         print (status._json)
         functs.json_dump(status._json,'isro.json')
         functs.csv_dump('isro.csv',status.id,status.created_at,status.text.encode('utf-8'))
